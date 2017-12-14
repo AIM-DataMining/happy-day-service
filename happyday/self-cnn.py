baf6c0cece7de8ddf3be4bff8ce58a8dc90859de
Oliver Fesseler <ofesseler@gmail.com>
2017-12-15 00:33:54 +0100

    selfmade cnn, ohne visualisierung

---
+++ happyday/self-cnn.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import tensorflow.contrib.keras as k
+from tensorflow.contrib.keras import layers, models, optimizers
+
+from matplotlib import pyplot as plt
+
+train_data_gen = k.preprocessing.image.ImageDataGenerator(
+    rescale=1./255.
+)
+
+test_data_gen = k.preprocessing.image.ImageDataGenerator(
+    rescale=1./255.
+)
+
+train_gen = train_data_gen.flow_from_directory(
+    "data/train",
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode="categorical"
+)
+
+test_gen = test_data_gen.flow_from_directory(
+    "data/test",
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode="categorical"
+)
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Dropout(0.25))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(layers.Dropout(0.25))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(2, activation='softmax'))
+
+adam = optimizers.Adam()
+model.compile(loss='categorical_crossentropy', optimizer=adam)
+model.fit_generator(
+    train_gen,
+    steps_per_epoch=200,
+    epochs=20,
+    validation_data=test_gen,
+    validation_steps=100
+)
+score = model.evaluate_generator(train_gen, steps=32)
