47d4030a059d677c7b3e2d4f1c07a2ba56cabf38
Oliver Fesseler <ofesseler@gmail.com>
2017-12-15 17:34:40 +0100

    adds tensorboard metrics
    Signed-off-by: Oliver Fesseler <ofesseler@gmail.com>

--- happyday/self-cnn.py
+++ happyday/self-cnn.py
@@ -47,11 +47,21 @@ model.add(layers.Dense(2, activation='softmax'))
 
 adam = optimizers.Adam()
 model.compile(loss='categorical_crossentropy', optimizer=adam)
+
+tbCallBack = k.callbacks.TensorBoard(
+    log_dir='./Graph/'+str(round(time.time() * 1000)),
+    histogram_freq=0,
+    write_graph=True,
+    write_images=True
+)
+
 model.fit_generator(
     train_gen,
     steps_per_epoch=200,
     epochs=20,
     validation_data=test_gen,
     validation_steps=100
+    validation_steps=10,
+    callbacks=[tbCallBack]
 )
 score = model.evaluate_generator(train_gen, steps=32)
