df059fb35b4ae103afef6a6c43560aad23e7eeba
Oliver Fesseler <ofesseler@gmail.com>
2018-01-01 03:43:09 +0100

    adds selfttest

---
+++ happyday/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='happy-day-service',
+    version='0.1',
+    packages=['TF_for_poets_script'],
+    package_dir={'': 'happyday'},
+    url='https://schrolm.de/happyday',
+    license='GPL',
+    author='oli',
+    author_email='oli@schrolm.de',
+    description='They\'re knowing your smile!'
+)
